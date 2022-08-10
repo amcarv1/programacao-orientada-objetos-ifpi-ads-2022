93c5ce28432da1d373153f9a56720b11e3e0de5e
Erick Amorim <erickamorimcarv@gmail.com>
2022-08-10 14:54:55 -0300

    revisão de javascript

--- 01-revisao-poo/ex06.js
+++ 01-revisao-poo/ex06.js
@@ -6,15 +6,29 @@ valor –1 é somente um terminador e não deve ser considerado nos cálculos.
 
 const prompt = require('prompt-sync')();
 
-let contador = Number(prompt('Digite o valor inicial: '));
+let counter = Number(prompt('Digite o valor inicial: '));
 
-let somatorio = 0;
-let quantidade = 0;
+let numbers = [];
+let sum = 0;
+let quantityNumbers = 0;
 
-while (contador > -1) {
-    somatorio += contador;
-    quantidade++;
-    contador--;
+while (counter > -1) {
+    numbers.push(counter);
+    sum += counter;
+    quantityNumbers++;
+    counter--;
 }
 
-console.log('Somatório:', somatorio,'\nMédia:', somatorio/quantidade);
+let average = sum/quantityNumbers;
+
+let sum2 = 0;
+let sum3 = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum2 = (numbers[i] - average) ** 2;
+    sum3 += sum2;
+}
+
+let standardDeviation = ((sum3 / quantityNumbers) ** 0.5);
+
+console.log('Somatório:', sum,'\nMédia:', average,'\nDesvio Padrão:', standardDeviation);
