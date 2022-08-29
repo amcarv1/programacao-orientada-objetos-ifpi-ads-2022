da9092df14703859eaee0206aab898a8a3871626
Erick Amorim <erickamorimcarv@gmail.com>
2022-08-29 17:55:01 -0300

    Resposta do Exercício 08

--- poo-atividade-01/ex08.ts
+++ poo-atividade-01/ex08.ts
@@ -1,5 +1,5 @@
-Questão 08) Crie uma classe Circulo que possua um atributo raio. Crie dois métodos que calculam a área e o perímetro. Instancie um objeto dessa classe, 
-atribua um valor ao raio e exiba a área e o perímetro chamando os dois métodos definidos.
+/* Questão 08) Crie uma classe Circulo que possua um atributo raio. Crie dois métodos que calculam a área e o perímetro. Instancie um objeto dessa classe, 
+atribua um valor ao raio e exiba a área e o perímetro chamando os dois métodos definidos. */
 
 class Circulo {
     raio: number = null;
