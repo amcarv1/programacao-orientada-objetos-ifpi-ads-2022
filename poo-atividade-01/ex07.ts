bdb5a6cdcc203fe14951dc8a588a3eb39b011dec
Erick Amorim <erickamorimcarv@gmail.com>
2022-08-29 17:54:29 -0300

    Resposta do Exercício 07

--- poo-atividade-01/ex07.ts
+++ poo-atividade-01/ex07.ts
@@ -1,5 +1,5 @@
-Questão 07) Considerando o exemplo da classe Retangulo dos slides, implemente um método adicional chamado que calcule o perímetro do retângulo e altere a classe
-TestaRetangulo para exibir o cálculo do perímetro.
+/* Questão 07) Considerando o exemplo da classe Retangulo dos slides, implemente um método adicional chamado que calcule o perímetro do retângulo e altere a classe
+TestaRetangulo para exibir o cálculo do perímetro. */
 
 class Retangulo {
     l1: number = 0;
