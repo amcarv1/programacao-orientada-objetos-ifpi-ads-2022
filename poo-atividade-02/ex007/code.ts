171f50e0d15a962b8d5b2e43053dc3757ae9cda2
Erick Amorim <erickamorimcarv@gmail.com>
2022-09-19 18:28:10 -0300

    Resposta

---
+++ poo-atividade-02/ex007/code.ts
@@ -0,0 +1,5 @@
+let nome: string = 'Ely';
+let pagamentoPorHora: number = 120.56;
+let linguagemPreferida: string = 'TypeScript';
+
+console.log(`${nome}\nMy paymente time is ${pagamentoPorHora}\nand\nmy preferred language is ${linguagemPreferida}`);
