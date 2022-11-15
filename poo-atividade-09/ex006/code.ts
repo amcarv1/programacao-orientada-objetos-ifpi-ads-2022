f5698cee86be6f420376dafd80fec7bbbfd968e4
Erick Amorim <erickamorimcarv@gmail.com>
2022-11-15 18:42:38 -0300

    Resolução

---
+++ poo-atividade-09/ex006/code.ts
@@ -0,0 +1,137 @@
+class AplicacaoError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+class ContaInexistenteError extends AplicacaoError {
+    constructor(message : string) {
+        super(message);
+    }
+}
+
+class ValorInvalidoError extends AplicacaoError {
+    constructor(message : string) {
+        super(message);
+    }
+}
+
+class PoupancaInvalidaError extends Error {
+    constructor(message : string) {
+        super(message);
+    }   
+}
+
+class Banco {
+    private _contas: Conta[] = [];
+
+    inserir(conta: Conta) {
+        this._contas.push(conta);
+    }
+
+    consultar(numero: string): Conta {
+        let contaProcurada!: Conta;
+        for (let i = 0; i < this._contas.length; i++) {
+            if (this._contas[i].numero == numero) {
+                contaProcurada = this._contas[i];
+            }   else {
+                throw new ContaInexistenteError('Conta Inexistente!');
+            }
+        }
+
+        return contaProcurada;
+    }
+
+    private consultarComFilter(numero: string): Conta {
+        return this._contas.filter(conta => conta.numero == numero)[0];
+    }
+
+    private consultarPorIndice(numero: string): number {
+        let indiceProcurado: number = -1;
+        for (let i = 0; i < this._contas.length; i++) {
+            if (this._contas[i].numero == numero) {
+                indiceProcurado = i;
+            }   else {
+                throw new ContaInexistenteError ('Conta Inexistente!');
+            }
+        }
+
+        return indiceProcurado;
+    }
+
+
+    alterar(conta: Conta) {
+        let indice: number = this.consultarPorIndice(conta.numero);
+        
+        this._contas[indice] = conta;
+    }
+
+    depositar(numero: string, valor: number): void {
+        let indice = this.consultarPorIndice(numero);
+
+        this._contas[indice].depositar(valor);
+    }
+
+    sacar(numero: string, valor: number): void {
+        let indice = this.consultarPorIndice(numero);
+
+        this._contas[indice].sacar(valor);   
+    }
+
+    excluir(numero: string): void {
+        let indice: number = this.consultarPorIndice(numero);
+
+        for (var i = indice; i < this._contas.length; i++) {
+            this._contas[i] = this._contas[i + 1];
+        }
+        
+        this._contas.pop();
+    }
+
+    transferir(numeroDebito: string, numeroCredito: string, valor: number) {
+		let contaCredito: Conta = this.consultar(numeroCredito);
+		let contaDebito: Conta = this.consultar(numeroDebito);
+		contaDebito.transferir(contaCredito, valor);
+	}
+
+	calcularQuantidadeContas(): number {
+		return this._contas.length;
+	}
+
+	calcularTotalSaldos(): number {
+		let totalSaldo: number = 0;
+		for (let conta of this._contas) {
+			totalSaldo += conta.saldo;
+		}
+
+		return totalSaldo;
+	}
+
+	calcularMediaSaldos() {
+		return this.calcularTotalSaldos() / this.calcularQuantidadeContas();
+	}
+
+    renderJuros(numero: string) {
+        let contaProcurada = this.consultar(numero);
+
+        if (contaProcurada instanceof Poupanca) {
+            contaProcurada.renderJuros();
+        }   else {
+            throw new PoupancaInvalidaError('O tipo da conta não é poupança!');
+        }
+    }
+
+    
+    listaContas(): string {
+        let listaStrings = '';
+        for(let i: number = 0; i < this._contas.length; i++) {
+            listaStrings = listaStrings + 
+                           ' Numero: ' + this._contas[i].numero +  
+                           ' - Saldo: '  + this._contas[i].saldo + '\n';
+                           
+        }   
+
+        return listaStrings;
+    }
+    
+}
