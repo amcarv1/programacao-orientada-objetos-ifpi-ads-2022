e4d51450c4af137b772a033712faecffca752d6f
Erick Amorim <erickamorimcarv@gmail.com>
2022-11-15 17:47:10 -0300

    Resolução

--- poo-atividade-09/ex001/code.ts
+++ poo-atividade-09/ex001/code.ts
@@ -1,1 +1,17 @@
+class AplicacaoError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
 
+class SaldoInsuficienteError extends AplicacaoError {
+    constructor(message : string) {
+        super(message);
+    }
+}
+
+class ContaInexistenteError extends AplicacaoError {
+    constructor(message : string) {
+        super(message);
+    }
+}
