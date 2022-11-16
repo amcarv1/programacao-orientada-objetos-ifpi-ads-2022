77b61365f978c82f409d0ade05c0c098e89f3d93
Erick Amorim <erickamorimcarv@gmail.com>
2022-11-15 21:54:16 -0300

    Resolução

---
+++ poo-atividade-10/ex010/code.ts
@@ -0,0 +1,42 @@
+class Conta {
+    private _nome: string;
+    private _saldo: number;
+
+    constructor(nome: string, saldo: number){
+        this._nome = nome;
+        this._saldo = saldo;
+    }
+
+
+    public get nome(): string{
+        return this._nome;
+    }
+
+    public setNome(nome: string): void {
+        this._nome = nome;
+    }
+
+    public get saldo(): number{
+        return this._saldo;
+    }
+
+    public setSaldo(saldo: number): void {
+        this._saldo = saldo;
+    }
+}
+
+interface Tributavel {
+    calculaTributos(): number;
+}
+
+class ContaCorrente extends Conta implements Tributavel{
+    public calculaTributos(): number {
+        return this.saldo * 0.1;
+    }
+}
+
+class SegurodeVida implements Tributavel {
+    public calculaTributos(): number {
+        return 50;
+    }
+}
